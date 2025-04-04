6bfd1e17913057fc392ef3a233609d2f789763eb
OlexiyHR <groleks30@gmail.com>
2025-04-04 22:41:15 +0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Hromiak_WPF_project;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
